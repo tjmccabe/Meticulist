30552cca733069603a437bd05b438044e4779e1e
tjmccabe <tjmccabe3@gmail.com>
2020-04-27 16:37:17 -0700

    Populate application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+    skip_before_action :verify_authenticity_token 
+    # REMOVE FOR PROD^^^
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+    
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logout!
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        if !logged_in?
+            render json: ["You must be logged in to see that content"]
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
         self.session_token ||= SecureRandom.urlsafe_base64
     end
 
-    def reset_session_token
+    def reset_session_token!
         self.session_token = SecureRandom.urlsafe_base64
         self.save!
         self.session_token
