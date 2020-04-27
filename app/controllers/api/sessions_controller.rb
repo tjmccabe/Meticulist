29c2648bf9257f3dbc9b5aaa7d46b21567d6f1bf
tjmccabe <tjmccabe3@gmail.com>
2020-04-27 16:51:55 -0700

    Populate users and sessions controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user.nil?
+            render json: ["Invalid credentials"], status: 422
+        else
+            login!(@user)
+            render "api/users/show"
+        end
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json: ["You are not logged in"], status 404
+        end
+    end
 end
