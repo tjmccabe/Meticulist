dc99ad0050e45f2c0f4a4a5cfde6170cf6eea740
tjmccabe <tjmccabe3@gmail.com>
2020-04-28 17:00:39 -0700

    Edit filename for Heroku

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import App from './app';
+import App from './App';
 
 const Root = ({store}) => (
     <Provider store={store}>
