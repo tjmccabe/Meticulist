ebbc32b8216812829f627334cb01b9e83f15284d
tjmccabe <tjmccabe3@gmail.com>
2022-12-03 19:09:38 -0500

    Upgrade Rails

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 require_relative "../app/middlewares/snake_case_parameters"
 
@@ -13,11 +13,12 @@ module Meticulist
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # config.middleware.use SnakeCaseParameters
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
