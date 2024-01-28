1f08d263f9578b7cf605036e04270ddd2c107dd5
TJ McCabe <tjmccabe3@gmail.com>
2024-01-28 14:19:37 -0500

    Separate css from js when compressing

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ module Meticulist
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    config.assets.js_compressor = Uglifier.new(:harmony => true)
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,12 @@ Rails.application.configure do
   # config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
-  config.assets.js_compressor = Uglifier.new(:harmony => true)
+  # config.assets.js_compressor = Uglifier.new(:harmony => true)
   # config.assets.css_compressor = :sass
+  config.assets.configure do |env|
+    env.js_compressor  = :uglifier # or :closure, :yui
+    env.css_compressor = :sass     # or :yui
+  end
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
